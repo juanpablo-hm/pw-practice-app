3bc74d02e3d99592a1c7a546f906f4fda5d25b5d
Juan Pablo HM <juanpablohm1995@gmail.com>
2026-09-25 10:40:35 -0600

    Methods Parametrization

--- tests/test-with-page-objects.spec.ts
+++ tests/test-with-page-objects.spec.ts
@@ -1,15 +1,24 @@
 import {test} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigation-page'
+import { FormLayoutsPage } from '../page-objects/form-layouts-page'
 
 
 test.beforeEach(async ({page}) => {
     await page.goto('/')
 })
 
-test('Navigate to From Layouts Page', async ( {page}) => {
+test('Navigate to From Layouts Page', async ({page}) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formLayoutsPage()
     await navigateTo.datePickerPage()
     await navigateTo.toasterPage()
     await navigateTo.smartTablePage()
+})
+
+test('Parametrized Page Objects Methods', async ({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const formLayoutsPage = new FormLayoutsPage(page)
+    await navigateTo.formLayoutsPage()
+    await formLayoutsPage.submitUsingTheGridForm('jp@mail.com', 'password123', 'Option 2' )
+    await formLayoutsPage.submitInlineForme('JP', 'jp@mail.com', false)
 })
