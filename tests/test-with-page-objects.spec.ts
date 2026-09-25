071e789b06ff6d1ae6a1d6d4028a99e8fa717986
Juan Pablo HM <juanpablohm1995@gmail.com>
2026-09-25 14:18:09 -0600

    Page Objects Manager

--- tests/test-with-page-objects.spec.ts
+++ tests/test-with-page-objects.spec.ts
@@ -1,29 +1,24 @@
-import {test} from '@playwright/test'
-import {NavigationPage} from '../page-objects/navigation-page'
-import { FormLayoutsPage } from '../page-objects/form-layouts-page'
-import {DatepickerPage} from '../page-objects/datepicker-page'
-
+import { test } from '@playwright/test'
+import { PageManager } from '../page-objects/page-manager'
 
 test.beforeEach(async ({page}) => {
     await page.goto('/')
 })
 
 test('Navigate to From Layouts Page', async ({page}) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datePickerPage()
-    await navigateTo.toasterPage()
-    await navigateTo.smartTablePage()
+    const pom = new PageManager(page)
+    await pom.navigateTo.formLayoutsPage()
+    await pom.navigateTo.datePickerPage()
+    await pom.navigateTo.toasterPage()
+    await pom.navigateTo.smartTablePage()
 })
 
 test('Parametrized Page Objects Methods', async ({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const formLayoutsPage = new FormLayoutsPage(page)
-    const datePickerPage = new DatepickerPage(page)
-    await navigateTo.formLayoutsPage()
-    await formLayoutsPage.submitUsingTheGridForm('jp@mail.com', 'password123', 'Option 2' )
-    await formLayoutsPage.submitInlineForme('JP', 'jp@mail.com', false)
-    await navigateTo.datePickerPage()
-    await datePickerPage.selectCommonDatepickerDateFromToday(5)
-    await datePickerPage.selectDatepickerWithRangeFromToday(7,15)
+    const pom = new PageManager(page)
+    await pom.navigateTo.formLayoutsPage()
+    await pom.formlayoutsPage.submitUsingTheGridForm('jp@mail.com', 'password123', 'Option 2' )
+    await pom.formlayoutsPage.submitInlineForme('JP', 'jp@mail.com', false)
+    await pom.navigateTo.datePickerPage()
+    await pom.datepickerPage.selectCommonDatepickerDateFromToday(5)
+    await pom.datepickerPage.selectDatepickerWithRangeFromToday(7,15)
 })
